01edbab082ceb0204c425f360d078eff24d15ca8
MEGARAJAN-S <megarajanmegan@gmail.com>
2025-04-01 23:40:26 +0530

    Atbash second changes

--- Traditional_Cryptography/Substitution_Ciphers/Single_Substitution/Monoalphabetical/AtbashCipher.java
+++ Traditional_Cryptography/Substitution_Ciphers/Single_Substitution/Monoalphabetical/AtbashCipher.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
 
 public class AtbashCipher {
+
     public static String encryptDecrypt(String input) {
         StringBuilder result = new StringBuilder();
 
         for (char ch : input.toCharArray()) {
             if (Character.isUpperCase(ch)) {
-                ch = (char) ('Z' - (ch - 'A'));
+                ch = (char) ('Z' - (ch - 'A')); 
             } else if (Character.isLowerCase(ch)) {
-                ch = (char) ('z' - (ch - 'a'));
+                ch = (char) ('z' - (ch - 'a')); 
             }
+
             result.append(ch);
         }
 
@@ -18,11 +20,16 @@ public class AtbashCipher {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter the Plain Text : ");
-        String text = input.nextLine();
-        String encryptedText = encryptDecrypt(text);
 
-        System.out.println("Original: " + text);
-        System.out.println("Encrypted/Decrypted: " + encryptedText);
+        System.out.print("Enter the Plain Text: ");
+        String text = input.nextLine();
+
+        String encryptedText = encryptDecrypt(text);
+        System.out.println("Encrypted Text: " + encryptedText);
+
+        String decryptedText = encryptDecrypt(encryptedText);
+        System.out.println("Decrypted Text: " + decryptedText);
+
+        input.close();
     }
 }
