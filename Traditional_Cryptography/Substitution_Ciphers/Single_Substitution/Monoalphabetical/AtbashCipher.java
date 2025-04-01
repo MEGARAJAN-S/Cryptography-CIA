37cc34e89f5ef382c9bc9345b107893e0125fe94
MEGARAJAN-S <megarajanmegan@gmail.com>
2025-04-01 23:16:14 +0530

    AtbashCipher

---
+++ Traditional_Cryptography/Substitution_Ciphers/Single_Substitution/Monoalphabetical/AtbashCipher.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class AtbashCipher {
+    public static String encryptDecrypt(String input) {
+        StringBuilder result = new StringBuilder();
+
+        for (char ch : input.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
+                ch = (char) ('Z' - (ch - 'A'));
+            } else if (Character.isLowerCase(ch)) {
+                ch = (char) ('z' - (ch - 'a'));
+            }
+            result.append(ch);
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Plain Text : ");
+        String text = input.nextLine();
+        String encryptedText = encryptDecrypt(text);
+
+        System.out.println("Original: " + text);
+        System.out.println("Encrypted/Decrypted: " + encryptedText);
+    }
+}
